82462bac253cbd71a81eca52724c6f692a485f05
Aimar Ibarra <82465645+AimarIbarra@users.noreply.github.com>
2024-03-13 09:45:03 +0100

    Update aia.hpp

--- aia.hpp
+++ aia.hpp
@@ -1,5 +1,5 @@
-#ifndef AALLOC_HPP_INCLUDED_P
-#define ALLLOC_HPP_INCLUDED_P
+#ifndef AIA_HPP_INCLUDED_P
+#define AIA_HPP_INCLUDED_P
 
 /* This program is free software: you can redistribute it and/or modify it under the terms of
  * the GNU General Public License as published by the Free Software Foundation, either
